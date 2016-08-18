0b2ce9c20a188ea4933329771b571e19311e0e4a
Molly LeCompte and Ryan Vinyard <molly@>
2016-08-18 11:02:28 -0700

    add script to create arraywith length of last digit of user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 //back end logic
 var userInput = "";
+var lastDig = "";
+var secondLastDig = parseInt(userInput.charAt(userInput.length-2));
+var thirdLastDig = parseInt(userInput.charAt(userInput.length-3));
+var fourthlastDig = parseInt(userInput.charAt(userInput.length-4));
+var array1 = [];
 
+var createArray1 = function(input) {
+  lastDig = parseInt(input.charAt(input.length-1));
+  array1.push("I".repeat(lastDig));
+  
 
+}
 
 
 //front end logic
-$("form").submit(function(event) {
-  event.preventDefault();
-  userInput = ("#user-input")
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    userInput = $("#user-input").val();
+    createArray1(userInput);
 
 
-
-
+  });
 });
