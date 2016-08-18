99d51b7baf491bfe38948f6315950f30e8320300
Molly LeCompte and Ryan Vinyard <molly@>
2016-08-18 14:48:58 -0700

    convert any number 1-3999 into Roman Numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 //back end logic
 var userInput = "";
 var lastDig = "";
-var secondLastDig = parseInt(userInput.charAt(userInput.length-2));
-var thirdLastDig = parseInt(userInput.charAt(userInput.length-3));
-var fourthlastDig = parseInt(userInput.charAt(userInput.length-4));
+var secondLastDig = "";
+var thirdLastDig = "";
+var fourthlastDig = "";
 var array1 = [];
+var array10 = [];
+var array100 = [];
 var populate1 = "";
-
+var populate10 = "";
+var populate100 = "";
+var populate1000 = "";
+var string1 = "";
+var string10 = "";
+var string100 = "";
 
 var createArray1 = function(input) {
   lastDig = parseInt(input.charAt(input.length-1));
@@ -22,7 +29,46 @@ var createArray1 = function(input) {
   else if (lastDig > 3) {
     array1.splice(1,4,"V");
   }
-alert(array1);
+  string1 = array1.join("");
+}
+
+var createArray10 = function(input) {
+  secondLastDig = parseInt(input.charAt(input.length-2));
+  populate10 = "X".repeat(secondLastDig);
+  array10 = populate10.split("");
+
+  if (secondLastDig > 8) {
+    array10.splice(-8,8,"C");
+  }
+  else if (secondLastDig >= 5) {
+    array10.splice(0,5,"L");
+  }
+  else if (secondLastDig > 3) {
+    array10.splice(1,4,"L");
+  }
+  string10 = array10.join("");
+}
+
+var createArray100 = function(input) {
+  thirdLastDig = parseInt(input.charAt(input.length-3));
+  populate100 = "C".repeat(thirdLastDig);
+  array100 = populate100.split("");
+
+  if (thirdLastDig > 8) {
+    array100.splice(-8,8,"M");
+  }
+  else if (thirdLastDig >= 5) {
+    array100.splice(0,5,"D");
+  }
+  else if (thirdLastDig > 3) {
+    array100.splice(1,4,"D");
+  }
+  string100 = array100.join("");
+}
+
+var create1000 = function(input) {
+  fourthLastDig = parseInt(input.charAt(input.length-4));
+  populate1000 = "M".repeat(fourthLastDig);
 }
 
 //front end logic
@@ -31,7 +77,9 @@ $(document).ready(function() {
     event.preventDefault();
     userInput = $("#user-input").val();
     createArray1(userInput);
-
-
+    createArray10(userInput);
+    createArray100(userInput);
+    create1000(userInput);
+    $("#numeral").text(populate1000 + string100 + string10 + string1);
   });
 });
