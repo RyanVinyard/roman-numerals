40d2e024054118d5bce310a4594f641af41cdbb6
Molly LeCompte and Ryan Vinyard <molly@>
2016-08-18 13:18:56 -0700

    turn single digit numbers into roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,25 @@ var secondLastDig = parseInt(userInput.charAt(userInput.length-2));
 var thirdLastDig = parseInt(userInput.charAt(userInput.length-3));
 var fourthlastDig = parseInt(userInput.charAt(userInput.length-4));
 var array1 = [];
+var populate1 = "";
+
 
 var createArray1 = function(input) {
   lastDig = parseInt(input.charAt(input.length-1));
-  array1.push("I".repeat(lastDig));
-  
+  populate1 = "I".repeat(lastDig);
+  array1 = populate1.split("");
 
+  if (lastDig > 8) {
+    array1.splice(-8,8,"X");
+  }
+  else if (lastDig >= 5) {
+    array1.splice(0,5,"V");
+  }
+  else if (lastDig > 3) {
+    array1.splice(1,4,"V");
+  }
+alert(array1);
 }
-
 
 //front end logic
 $(document).ready(function() {
